f5250d2594b38653cad437b00b5f07c12ffbfe49
Luu The Vinh <luuthevinh1988@gmail.com>
2022-11-23 22:34:37 +0700

    Sidebar and ContextProvider

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
+import { ContextProvider } from './contexts/ContextProvider'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>, document.getElementById("root"));
